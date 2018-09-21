ad1450fc6b0097278de6d86b3514b1d402d7a970
dylan <dylan.deleeuw@live.nl>
2018-09-21 09:12:35 +0200

    fixed edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,23 @@ use Lcobucci\JWT\Parser;
 class UserController extends Controller
 {
     public function editUser(Request $request){
-    	User::where('id', $request->id)
+    	//dd($request);
+    	$token = explode(' ', $request->header('Authorization'))[1];
+
+    	//dd($token);
+        
+        $token = (new Parser())->parse($token); // Parses from a string
+        $data = new ValidationData();
+
+       $id = $token->getClaim("id");
+
+    	User::where('id', $id)
     		->update(['email' => $request->email,
     				  'name' => $request->name]);
+
+    	return response([
+	            'status' => 'success'
+	        ]);
     }
 
     public function getUser(Request $request){

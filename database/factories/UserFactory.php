0e917de6cfac88f2db3b8b63b9a999a1b3a6b6d4
DefaultUser <menno.schreij@hotmail.com>
2018-10-10 09:28:50 +0200

    Update database/factories/UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->unique()->userName,
         'email' => $faker->unique()->safeEmail,
+        'role' => 1,
         'password' => Hash::make('welkom01'),//'$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];

da2f8ac70c7d240fa5377c58b59fe684ad51610f
dylan <dylan.deleeuw@live.nl>
2018-10-17 09:28:24 +0200

    usercontrolller sends skills with student

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,24 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Token;
+use App\Skill;
 use Illuminate\Http\Request;
 use App\User;
 use App\Student;
 use App\Company;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function editUser(Request $request){
-       $id = $request->auth["id"];
-       $role = $request->auth["role"];
+        $id = $request->auth["id"];
+        $role = $request->auth["role"];
 
         User::where('id', $id)
-    		->update(['email' => $request->email,
-                    'name' => $request->name,
-                    'theme' => $request->theme,
-                    'city' => $request->city,
-                    'street' => $request->street,
-                    'house_number' => $request->house_number]);
+            ->update(['email' => $request->email,
+                'name' => $request->name,
+                'theme' => $request->theme,
+                'city' => $request->city,
+                'street' => $request->street,
+                'house_number' => $request->house_number]);
 
         if ($role == '1'){
             Student::where('user_id', $id)
@@ -48,10 +50,19 @@ class UserController extends Controller
         $role = $request->auth["role"];
 
         if ($role == '1'){
-           $user = Student::where('user_id', $id)
-               ->join('users', 'students.user_id', '=', 'users.id')
-               ->select('users.role', 'users.id', 'users.name', 'users.email', 'users.theme', 'users.city', 'users.street', 'users.house_number', 'students.school', 'students.date_of_birth', 'students.gender')
-               ->get();
+            $user = Student::where('user_id', $id)
+                ->join('users', 'students.user_id', '=', 'users.id')
+                ->select('users.role', 'users.id', 'users.name', 'users.email', 'users.theme', 'users.city', 'users.street', 'users.house_number', 'students.id AS student_id',
+                    'students.school', 'students.date_of_birth', 'students.gender')
+                ->get();
+
+            $skills = DB::table('student_skill')
+                ->where('student_id', $user[0]->student_id)
+                ->join('skills', 'skills.id', '=', 'student_skill.skill_id')
+                ->select('student_skill.skill_id AS id', 'skills.name', 'student_skill.level')
+                ->get();
+
+            $user[0]->skills = $skills;
 
         } elseif ($role == '2'){
             $user = Company::where('user_id', $id)

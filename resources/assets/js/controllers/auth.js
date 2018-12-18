a05417deaafb8b977681a6b25ab0c03ccc7a5632
Kaan <99040235@mydavinci.nl>
2018-12-18 10:51:57 +0100

    Fix #57

--- resources/assets/js/controllers/auth.js
+++ resources/assets/js/controllers/auth.js
@@ -1,31 +1,31 @@
 import http from '@/core/http';
 import router from '../router';
 import { runtime } from '../core/runtime';
-import { Validations, types, between, required } from '@/core/Validation';
+// import { Validations, types, between, required } from '@/core/Validation';
 
-const login_requirements = {
-    email: {
-        required,
-        type: types.email
-    },
-    password: {
-        required,
-        type: types.string,
-        length: between(6, 20)
-    }
-};
+// const login_requirements = {
+//     email: {
+//         required,
+//         type: types.email
+//     },
+//     password: {
+//         required,
+//         type: types.string,
+//         length: between(6, 20)
+//     }
+// };
 
-const register_requirements = {
-    email: {
-        required,
-        type: types.email
-    },
-    password: {
-        required,
-        type: types.string,
-        length: between(6, 20)
-    }
-};
+// const register_requirements = {
+//     email: {
+//         required,
+//         type: types.email
+//     },
+//     password: {
+//         required,
+//         type: types.string,
+//         length: between(6, 20)
+//     }
+// };
 
 export default class Auth {
     static login (credentials) {  
@@ -46,20 +46,20 @@ export default class Auth {
     static register (credentials) {
 
 
-        const result = new Validation(register_requirements, {});
+        // const result = new Validation(register_requirements, {});
 
-        // http.post(`auth/login`, credentials)
-        //     .then(res => {
-        //         localStorage.setItem("accessToken", res.data.token);
-        //         this.$parent.read_token();
-        //         this.$router.push({ name: "main" });
-        //     })
-        //     .catch(err => {
-        //         this.error = {
-        //         show: true,
-        //         message: "Invalid credentials"
-        //     }
-        // });
+        http.post(`auth/login`, credentials)
+            .then(res => {
+                localStorage.setItem("accessToken", res.data.token);
+                this.$parent.read_token();
+                this.$router.push({ name: "main" });
+            })
+            .catch(err => {
+                this.error = {
+                show: true,
+                message: "Invalid credentials"
+            }
+        });
     }
 
     static logout (redirect = true) {

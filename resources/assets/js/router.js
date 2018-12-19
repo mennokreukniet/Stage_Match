b41e838acf8e285bac193406884d1688b7dfa486
Kaan <99040235@mydavinci.nl>
2018-12-19 10:03:26 +0100

    Added token validation to middleware

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -8,12 +8,6 @@ const router = new VueRouter(routes);
  * @description Router middleware
  */
 router.beforeEach((to, from, next) => {
-    // if (JSON.parse(atob(localStorage.getItem("accessToken").split(".")[1])).exp < Math.floor(Date.now()/1000)) {
-    //     console.log(1)
-    //     } else {
-    //         console.log(2)
-    //     }
-
     document.title = `StageMatch - ${to.name}`;
 
     const token = localStorage.getItem("accessToken");
@@ -36,6 +30,10 @@ router.beforeEach((to, from, next) => {
 
     if (token === null && to.meta.auth_not_allowed !== true) {
         return next({ name: "index" });
+    }
+
+    if (token !== null && JSON.parse(atob(token.split(".")[1])).exp < Math.floor(Date.now()/1000)) {
+        return Auth.logout();
     }
 
     next();
